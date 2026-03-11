e7aef4c0a6c41d36df93704c9465a131c7c43767
심지훈 <wlgns2223@gmail.com>
2026-03-11 22:25:53 +0900

    feat: entity definition

---
+++ src/main/java/com/sparta/msa/lesson/domain/user/entity/User.java
@@ -0,0 +1,56 @@
+package com.sparta.msa.lesson.domain.user.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import java.time.LocalDateTime;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.experimental.FieldDefaults;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.DynamicInsert;
+import org.hibernate.annotations.DynamicUpdate;
+import org.hibernate.annotations.UpdateTimestamp;
+
+@Getter
+@Entity
+@Table(name = "users")
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@FieldDefaults(level = AccessLevel.PRIVATE)
+@DynamicInsert
+@DynamicUpdate
+public class User {
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  Long id;
+
+  @Column(nullable = false, length = 50)
+  String name;
+
+  @Column(nullable = false, unique = true)
+  String email;
+
+  @Column(nullable = false)
+  String password;
+
+  @Column(nullable = false, updatable = false)
+  @CreationTimestamp
+  LocalDateTime createdAt;
+
+  @Column(nullable = false)
+  @UpdateTimestamp
+  LocalDateTime updatedAt;
+
+  @Builder
+  private User(String name, String email, String password) {
+    this.name = name;
+    this.email = email;
+    this.password = password;
+  }
+}
